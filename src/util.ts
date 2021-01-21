e4a1ed6dd172ab3ddc768b5b81aa0467d19e773d
Tom Madelin + Matt Ocovsky <tom.madelin@gmail.com>
2021-01-21 06:48:43 +0000

    update util

--- src/util.ts
+++ src/util.ts
@@ -18,8 +18,31 @@ let R = 6371e3; // Earth's radius in km
 let d = 5000; // Distance travelled
 let bearing = 98 * Math.PI/180; // Clockwise from north in radians
 
+/*
+    This uses the ‘haversine’ formula to calculate the great-circle distance between two points – that is, the shortest distance over the earth’s surface – giving an ‘as-the-crow-flies’ distance between the points (ignoring any hills they fly over, of course!).
+*/
+// let lat1=0;
+// let lat2=0;
+// let lon1=0;
+// let lon2=0;
+
+// const φ1 = currentLat * Math.PI/180; // φ, λ in radians
+// const φ2 = lat2 * Math.PI/180;
+// const Δφ = (lat2-currentLat) * Math.PI/180;
+// const Δλ = (lon2-currentLong) * Math.PI/180;
+
+// const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+// const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+// const d = R * c; // in metres
+
+const currentLatRads = currentLat * Math.PI/180;
+const currentLonRads = currentLong * Math.PI/180; // This may be wrong...
+
 const destLat = Math.asin(Math.sin(currentLat)*Math.cos(d/R) + Math.cos(currentLat)*Math.sin(d/R)*Math.cos(bearing));
 const destLong = currentLong + Math.atan2(Math.sin(bearing)*Math.sin(d/R)*Math.cos(currentLat),Math.cos(d/R)-Math.sin(currentLat)*Math.sin(destLat));
 
-console.log(destLat);
-console.log(destLong);
+console.log(currentLatRads);
+console.log(currentLonRads);
+
+// export {}
